297ae172a4136c1e88f23e0015727196c26fcd27
rexbigsun <644254833@qq.com>
2016-06-25 11:35:29 +0800

    exp1 tree&who

---
+++ tree/tree.c
@@ -0,0 +1,41 @@
+﻿#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <dirent.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <stdlib.h>
+
+void scan_dir(char *dir, int depth)
+{
+   DIR *dp;
+   struct dirent *entry;
+   struct stat statbuf;
+   if((dp = opendir(dir)) == NULL) {
+      puts("Cannot open directioy");
+      return;
+   }
+   chdir(dir);
+   while((entry = readdir(dp)) != NULL) {
+      lstat(entry->d_name, &statbuf);
+      if(S_IFDIR & statbuf.st_mode) {
+         if(strcmp(".", entry->d_name) == 0 || strcmp("..", entry->d_name) == 0)
+        continue;
+         printf("%*s%s/\n",depth,"",entry->d_name);
+         scan_dir(entry->d_name,depth+4);
+      }
+      else
+         printf("%*s%s\n", depth, "", entry->d_name);
+   }
+   chdir("..");
+   closedir(dp);
+}
+
+int main(int argc, char *argv[])
+{
+    puts("Scan the directory:");
+    scan_dir(argv[1], 0);
+    puts("Scanning ended.");
+    return 0;
+}
---
+++ who/who0.c
@@ -0,0 +1,35 @@
+#include	<stdlib.h>
+#include	<stdio.h>
+#include	<fcntl.h>
+#include	<utmp.h>
+
+/*
+ *	who version 0
+ *		main outline but no substance
+ */
+
+int
+main()
+{
+        int         fd;                 /* for file des of utmp */
+        struct utmp current_record;     /* hold info from file */
+        int         reclen = sizeof(struct utmp);
+
+        fd = open(UTMP_FILE, O_RDONLY);
+        if ( fd == -1 ){
+                perror( "who0" );       /* report a system error */
+                exit(1);                /* and get out now       */
+        }
+
+        while ( read(fd, &current_record, reclen) == reclen )
+                show_info( &current_record );
+
+        close (fd);
+        return 0;
+}
+
+/* dummy function */
+show_info( struct utmp *utbufp )
+{
+	printf("show_info() display messages here ...\n");	/* to be realized */
+}
